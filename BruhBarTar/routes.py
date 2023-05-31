45f2b4112acd49277840df4096e98b7072b64dc6
user1 <user1@A518-09>
2023-05-31 14:50:37 +0300

    Сделана страница оформленных заказов.

--- BruhBarTar/routes.py
+++ BruhBarTar/routes.py
@@ -54,4 +54,9 @@ def companies():
         year=datetime.now().year
     )
 
+@route('/orders')
+def orders():
+    with open('orders.json') as file:
+        data = json.load(file)
+    return template('orders', orders=orders, title='Вклад капибар в культуру человека', year=datetime.now().year, data=data)
 
